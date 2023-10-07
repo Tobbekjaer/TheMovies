f0d7a2d795151e0896d2be41d04a2967aeb20690
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-10-07 11:50:29 +0200

    Scenarie 1 done

--- TheMovies/UI/RegisterMovieWindow.xaml.cs
+++ TheMovies/UI/RegisterMovieWindow.xaml.cs
@@ -29,12 +29,14 @@ namespace TheMovies.UI
 
         private void btnConfirm_Click(object sender, RoutedEventArgs e)
         {
+            // Calls AddMovie in Controller class
             controller.AddMovie(tbTitel.Text, Convert.ToInt32(tbDuration.Text), tbGenre.Text);
             ClearTextBoxes();
         }
 
         private void ClearTextBoxes()
         {
+            // Clears all text boxes
             tbTitel.Clear();
             tbDuration.Clear();
             tbGenre.Clear();
