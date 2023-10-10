f80f1c8b8887406d8f838f42014fbd2667fe68c0
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-10-10 15:34:48 +0200

    Added a Cinema class

---
+++ TheMovies/Model/Cinema.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies.Model
+{
+    public class Cinema
+    {
+        public string CinemaName { get; set; }
+        public int CinemaHall { get; set; }
+
+        public Cinema(string cinemaName, int cinemaHall)
+        {
+            CinemaName = cinemaName;
+            CinemaHall = cinemaHall;
+        }
+    }
+}
