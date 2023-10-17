278b20cb5ccc02909ba2e2885c0cbc86fb511f5d
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-10-17 09:42:03 +0200

    Added Booking class

---
+++ TheMovies/Model/Booking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies.Model
+{
+    public class Booking
+    {
+        public int TicketAmount { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Show Show { get; set; }
+        public int GeneratedBookingID { get; set; }
+
+        public Booking(int ticketAmount, string email, string phone, Show show)
+        {
+            TicketAmount = ticketAmount;
+            Email = email;
+            Phone = phone;
+            Show = show;
+        }
+
+
+    }
+}
